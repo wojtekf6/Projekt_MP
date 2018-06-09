b40f63c1a440bd9669eae0a80829926705547252
Wojtek <wojtekf6@gmail.com>
2018-06-09 15:44:47 +0200

    Initial Commit

---
+++ Projekt_MP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projekt_MP
+//
+//  Created by Wojtek Filipowicz on 09/06/2018.
+//  Copyright © 2018 Wojtek Filipowicz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
